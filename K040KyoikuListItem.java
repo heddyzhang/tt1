4773fb605a9b3b366bc6b83d8fd2a1f1e4859d5a
heddyzhang <heddyzhangt@gmail.com>
2018-12-21 17:05:37 +0900

    Add files via upload

---
+++ K040KyoikuListItem.java
@@ -0,0 +1,37 @@
+package jp.co.toshiba.traces.entity.k;
+
+public class K040KyoikuListItem {
+
+	/** 実施年月日 */
+	public String kyoikuDate;
+
+	/** 管理番号（中登）*/
+	public String chutoNo;
+
+	/** 氏名 */
+	public String fullname;
+
+	/** 実施者名 */
+	public String jishiMei;
+
+	/** 科目名 */
+	public String kamokuMei;
+
+	/** 免除 */
+	public String menjoFlg;
+
+	/** 施設名 */
+	public String shisetsuMei;
+
+	/** d/E教育終了日 */
+	public String eKyoikuDate;
+
+	/** d/E教育日数 */
+	public String eKyoikuDu;
+
+	/** 入力(更新）日時 */
+	public String kosinDate;
+
+	/** 入力者 */
+	public String kosinName;
+}
